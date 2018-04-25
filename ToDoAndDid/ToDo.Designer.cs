47744ba4c884af352642d1e14fc4a64fa12a2662
villar35738 <danielvillar1998@outlook.com>
2018-04-25 11:46:06 -0300

    PRIMEIRO COMMIT, TELA DE ToDo PRONTA

---
+++ ToDoAndDid/ToDo.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ToDoAndDid
+{
+    partial class ToDo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.BtnRemove = new System.Windows.Forms.Button();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.listNames = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(2, 190);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(78, 51);
+            this.btnEdit.TabIndex = 21;
+            this.btnEdit.Text = "Editar Selecionada";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // BtnRemove
+            // 
+            this.BtnRemove.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnRemove.Location = new System.Drawing.Point(254, 190);
+            this.BtnRemove.Name = "BtnRemove";
+            this.BtnRemove.Size = new System.Drawing.Size(78, 51);
+            this.BtnRemove.TabIndex = 17;
+            this.BtnRemove.Text = "Deletar Selecionada";
+            this.BtnRemove.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.BtnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(86, 190);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(78, 51);
+            this.btnAddUser.TabIndex = 14;
+            this.btnAddUser.Text = "Incluir Nova Tarefa";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(170, 190);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(78, 51);
+            this.btnCopy.TabIndex = 15;
+            this.btnCopy.Text = "Finalizar Tarefa";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // listNames
+            // 
+            this.listNames.FormattingEnabled = true;
+            this.listNames.HorizontalScrollbar = true;
+            this.listNames.Location = new System.Drawing.Point(4, 37);
+            this.listNames.Name = "listNames";
+            this.listNames.ScrollAlwaysVisible = true;
+            this.listNames.Size = new System.Drawing.Size(496, 147);
+            this.listNames.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Tarefas a fazer:";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(338, 190);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(78, 51);
+            this.btnLimpar.TabIndex = 16;
+            this.btnLimpar.Text = "Limpar Tudo";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(126, 11);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(374, 20);
+            this.txbName.TabIndex = 13;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnPesquisar.Location = new System.Drawing.Point(422, 190);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(78, 51);
+            this.btnPesquisar.TabIndex = 18;
+            this.btnPesquisar.Text = "Busca";
+            this.btnPesquisar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnPesquisar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // ToDo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 248);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.BtnRemove);
+            this.Controls.Add(this.btnAddUser);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.listNames);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.btnPesquisar);
+            this.Name = "ToDo";
+            this.Text = "Tarafas a fazer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button BtnRemove;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.Button btnCopy;
+        internal System.Windows.Forms.ListBox listNames;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.Button btnPesquisar;
+    }
+}
+
---
+++ ToDoAndDid/ToDo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoAndDid
+{
+    public partial class ToDo : Form
+    {
+        public ToDo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
